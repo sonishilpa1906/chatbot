2bddf6cc6e39f38fa5bfef8c7bd7789191a8c892
sonishilpa1906 <sonishilpa1906@gmail.com>
2026-08-18 17:53:20 +0530

    maintain chat history through langchain messages

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,22 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI()
 
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant")
+]
+
 while True:
     user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
     if(user_input == "exit"):
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
     print(f"AI: {result.content}")
+
+print(chat_history)
